a942a49b90d077c7495ed238a31623dc0d96ea60
lijunhao <1712240212@qq.com>
2026-01-18 18:16:15 +0800

    code update

--- 03_WebAutomation/webAuto_prj/codeDay04/A/06 文件上传.py
+++ 03_WebAutomation/webAuto_prj/codeDay04/A/06 文件上传.py
@@ -1,13 +1,36 @@
-from common.open_web import *
+from codeDay04.A.common.open_web import *
 from selenium.webdriver.common.by import By
 from os import path
 
 
-file_name = path.abspath('../pages/example.html')
-driver = open_web(file_name)
+file = path.abspath('../pages/example.html')
+driver = open_web(file)
 
 # 点击文件上传按钮
-driver.find_element(By.NAME, 'attach[]').send_keys(R'C:\Users\Administrator\Desktop\logwatch.exe')
+# driver.find_element(By.NAME, 'attach[]').send_keys(R'C:\Users\Administrator\Desktop\logwatch.exe')
 
-sleep(2)
+
+def upload(by, el_path, file_path):
+    driver.find_element(by, el_path).send_keys(file_path)
+
+
+loc = (By.NAME, 'attach[]')
+# upload(*loc, r'C:\Users\Administrator\Desktop\logwatch.exe')
+# upload(*loc, path.abspath('../pages/example.html'))
+# upload(*loc, '../pages/example.html')   # error：path is not absolute: ../pages/example.html
+
+# filename = 'example.html'
+filename = r'C:\Users\Administrator\Desktop\logwatch.exe'
+
+if ':' not in filename:
+    # 认为只输入文件名
+    full_path = path.join(path.abspath('../pages'), filename)
+    upload(*loc, full_path)
+else:
+    # 则认为输入绝对路径, 直接输入路径需要在路径前加 r
+    upload(*loc, filename)
+    for i in filename:
+        print(i, end=' ')
+
+sleep(1)
 driver.quit()
